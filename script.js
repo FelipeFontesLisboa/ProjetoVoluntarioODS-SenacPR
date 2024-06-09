f3aa59891580683df1790d7f7144b3c2d853033d
felipefonteslisboa <felipefonteslisboa@gmail.com>
2024-06-09 01:37:37 -0300

    finalização modal

--- script.js
+++ script.js
@@ -92,3 +92,25 @@ function initScrollSuave() {
 initScrollSuave();
 
 //-------------------------------------------------------------------------------
+
+function initModal() {
+
+
+const bntColaboradores = document.querySelector('.buttonColaboradores')
+const bntCloseModal = document.querySelector('.closeModal')
+const modal = document.querySelector('.modal')
+
+function showModal() {
+   modal.classList.add('ativa')
+}
+function closedModal(){
+  modal.classList.remove('ativa')
+}
+modal.addEventListener('click', ()=>{
+  closedModal()
+})
+bntCloseModal.addEventListener('click', closedModal)
+bntColaboradores.addEventListener('click', showModal)
+}
+
+initModal()
