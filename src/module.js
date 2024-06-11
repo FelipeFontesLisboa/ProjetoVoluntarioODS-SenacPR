e30409f69bed63270496bc18f747b1c44a77d969
felipefonteslisboa <felipefonteslisboa@gmail.com>
2024-06-11 18:05:36 -0300

    erro arquivo module

--- src/module.js
+++ src/module.js
@@ -29,21 +29,24 @@ export  function initTabNav() {
 export  function initAccordion() {
     const accordionList = document.querySelectorAll(".js-accordion dt");
     const ativaClass = "ativo"; 
+    
     if (accordionList.length) {
       accordionList[0].classList.add(ativaClass);
       accordionList[0].nextElementSibling.classList.add(ativaClass);
   
       function activeAccordion() {
-    
         this.classList.toggle(ativaClass); 
         this.nextElementSibling.classList.toggle(ativaClass); 
       }
       accordionList.forEach((item) => {
         item.addEventListener("click", activeAccordion);
+     
       });
     }
   }
   initAccordion();
+
+  
   //Animação ao scroll  section ------------------------------------------------------
 
 export  function initAnimationScrrol() {
---
+++ src/scriptModule.js
@@ -0,0 +1,12 @@
+
+import { initTabNav } from "./src/module.js";
+import { initAccordion } from "./src/module.js";
+import { initAnimationScrrol } from "./src/module.js";
+import { initScrollSuave } from "./src/module.js";
+import { initModal } from "./src/module.js";
+
+initTabNav();
+initAccordion();
+initAnimationScrrol();
+initScrollSuave();
+initModal();
